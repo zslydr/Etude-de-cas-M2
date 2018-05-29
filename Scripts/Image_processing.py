2116d50da29e9ee175257da72d0643818777047d
zslydr <30627785+zslydr@users.noreply.github.com>
2018-05-29 18:53:04 +0200

    update de structure

---
+++ Scripts/Image_processing.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 29 18:43:26 2018
+
+@author: Raphael
+"""
+
+import os
+import pickle
+import importlib
+import numpy as np
+import matplotlib.pyplot as plt
+os.chdir('/Users/Raphael/Github/Etude-de-Cas-M2/Scripts') #Select your working directory
+cwd = os.getcwd()
+Functions=importlib.import_module("Functions")
+Functions=importlib.reload(Functions)
+#%%
+# Charger les données brutes
+
+data_path="/Users/Raphael/Mes cours/Magistere 3eme année/Etude de Cas/data/"
+#path="C:/Users/Pierre Lavigne/Desktop/GTSRB/Final_Training/Images/"
+trainImages, trainLabels = Functions.readTrafficSigns(data_path+"GTSRB/Final_Training/Images/")
+trainImages=np.array(trainImages)
+#%%
+with open(data_path+'trainImages.pickle', 'wb') as f:
+    pickle.dump(trainImages, f, protocol=pickle.HIGHEST_PROTOCOL)
+    
+with open(data_path+'trainLabels.pickle', 'wb') as f:
+    pickle.dump(trainLabels, f, protocol=pickle.HIGHEST_PROTOCOL)
+    
+trainImages_resized=np.array([Functions.resize_sign(x,(32,32)) for x in trainImages])
+
+del trainImages
+
+#%%
+with open('/Users/Raphael/Github/Etude-de-Cas-M2/data/trainImages_resized.pickle', 'wb') as f:
+    pickle.dump(trainImages_resized, f, protocol=pickle.HIGHEST_PROTOCOL)
--- Scripts/neural_network.py
+++ Scripts/neural_network.py
@@ -7,18 +7,37 @@ Created on Tue May  8 16:45:55 2018
 """
 
 import os
+import pickle
 import importlib
-
+import numpy as np
 import matplotlib.pyplot as plt
 os.chdir('/Users/Raphael/Github/Etude-de-Cas-M2/Scripts') #Select your working directory
 cwd = os.getcwd()
 Functions=importlib.import_module("Functions")
 Functions=importlib.reload(Functions)
 #%%
+# Charger les données brutes
 
-data_path="/Users/Raphael/Mes cours/Magistere 3eme année/Etude de Cas/data/GTSRB/Final_Training/Images/"
+data_path="/Users/Raphael/Mes cours/Magistere 3eme année/Etude de Cas/data/"
 #path="C:/Users/Pierre Lavigne/Desktop/GTSRB/Final_Training/Images/"
-trainImages, trainLabels = Functions.readTrafficSigns(data_path)
+
+def load_pickled_data(file, columns):
+    with open(file, mode='rb') as f:
+        dataset = pickle.load(f)
+    return tuple(map(lambda c: dataset[c], columns))
 #%%
 
-trainImages_resized=[Functions.resize_sign(x,(32,32)) for x in trainImages]
+
+X_train, y_train = load_pickled_data(data_path+"train.p", ['features', 'labels'])
+
+
+#%%
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+model = ResNet50(weights='imagenet')
+
+preds = model.predict(x)
+
