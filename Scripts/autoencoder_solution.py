8b1eb85eb9e6f76cb8c109760c267d7e113e9525
Romainmarmorat <39861718+Romainmarmorat@users.noreply.github.com>
2018-06-24 13:43:01 +0200

    Add files via upload

--- Scripts/autoencoder_solution.py
+++ Scripts/autoencoder_solution.py
@@ -6,6 +6,118 @@ Ceci est un script temporaire.
 """
 
 #%%
+
+import os
+import pickle
+import importlib
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.transform import rotate
+from skimage.transform import warp
+from skimage.transform import ProjectiveTransform
+import random
+
+
+def load_pickled_data(file, columns):
+    with open(file, mode='rb') as f:
+        dataset = pickle.load(f)
+    return tuple(map(lambda c: dataset[c], columns))
+
+
+def preprocess_dataset(X):
+    #Convert to grayscale, e.g. single Y channel
+    X = 0.299 * X[:, :, :, 0] + 0.587 * X[:, :, :, 1] + 0.114 * X[:, :, :, 2]
+    #Scale features to be in [0, 1]
+    X = (X / 255.).astype(np.float32)
+
+    # Add a single grayscale channel
+    X = X.reshape(X.shape + (1,)) 
+    return X
+
+def flip_extend(X, y):
+    # Classes of signs that, when flipped horizontally, should still be classified as the same class
+    self_flippable_horizontally = np.array([11, 12, 13, 15, 17, 18, 22, 26, 30, 35])
+    # Classes of signs that, when flipped vertically, should still be classified as the same class
+    self_flippable_vertically = np.array([1, 5, 12, 15, 17])
+    # Classes of signs that, when flipped horizontally and then vertically, should still be classified as the same class
+    self_flippable_both = np.array([32, 40])
+    # Classes of signs that, when flipped horizontally, would still be meaningful, but should be classified as some other class
+    cross_flippable = np.array([
+        [19, 20], 
+        [33, 34], 
+        [36, 37], 
+        [38, 39],
+        [20, 19], 
+        [34, 33], 
+        [37, 36], 
+        [39, 38],   
+    ])
+    num_classes = 43
+    
+    X_extended = np.empty([0, X.shape[1], X.shape[2], X.shape[3]], dtype = X.dtype)
+    y_extended = np.empty([0], dtype = y.dtype)
+    
+    for c in range(num_classes):
+        # First copy existing data for this class
+        X_extended = np.append(X_extended, X[y == c], axis = 0)
+        # If we can flip images of this class horizontally and they would still belong to said class...
+        if c in self_flippable_horizontally:
+            # ...Copy their flipped versions into extended array.
+            X_extended = np.append(X_extended, X[y == c][:, :, ::-1, :], axis = 0)
+        # If we can flip images of this class horizontally and they would belong to other class...
+        if c in cross_flippable[:, 0]:
+            # ...Copy flipped images of that other class to the extended array.
+            flip_class = cross_flippable[cross_flippable[:, 0] == c][0][1]
+            X_extended = np.append(X_extended, X[y == flip_class][:, :, ::-1, :], axis = 0)
+        # Fill labels for added images set to current class.
+        y_extended = np.append(y_extended, np.full((X_extended.shape[0] - y_extended.shape[0]), c, dtype = int))
+        
+        # If we can flip images of this class vertically and they would still belong to said class...
+        if c in self_flippable_vertically:
+            # ...Copy their flipped versions into extended array.
+            X_extended = np.append(X_extended, X_extended[y_extended == c][:, ::-1, :, :], axis = 0)
+        # Fill labels for added images set to current class.
+        y_extended = np.append(y_extended, np.full((X_extended.shape[0] - y_extended.shape[0]), c, dtype = int))
+        
+        # If we can flip images of this class horizontally AND vertically and they would still belong to said class...
+        if c in self_flippable_both:
+            # ...Copy their flipped versions into extended array.
+            X_extended = np.append(X_extended, X_extended[y_extended == c][:, ::-1, ::-1, :], axis = 0)
+        # Fill labels for added images set to current class.
+        y_extended = np.append(y_extended, np.full((X_extended.shape[0] - y_extended.shape[0]), c, dtype = int))
+    
+    return (X_extended, y_extended)
+
+#%%
+(images, labels) = load_pickled_data("C:/Users/Pierre Lavigne/Desktop/data/train.p", ['features', 'labels'])
+
+#%%
+
+images_data = []
+labels_data = []
+
+n = 0
+image_size = images[0].shape[0]
+for i in range(images.shape[0]):
+    d = random.uniform(-15,15)
+    tl_top = random.uniform(-d, d)     # Top left corner, top margin
+    tl_left = random.uniform(-d, d)    # Top left corner, left margin
+    bl_bottom = random.uniform(-d, d)  # Bottom left corner, bottom margin
+    bl_left = random.uniform(-d, d)    # Bottom left corner, left margin
+    tr_top = random.uniform(-d, d)     # Top right corner, top margin
+    tr_right = random.uniform(-d, d)   # Top right corner, right margin
+    br_bottom = random.uniform(-d, d)  # Bottom right corner, bottom margin
+    br_right = random.uniform(-d, d)   # Bottom right corner, right margin
+    transform = ProjectiveTransform()
+    transform.estimate(np.array(((tl_left, tl_top),(bl_left, image_size - bl_bottom),(image_size - br_right, image_size - br_bottom),
+        (image_size - tr_right, tr_top))), np.array(((0, 0),(0, image_size),(image_size, image_size),(image_size, 0))))
+
+    delta = random.uniform(-20,20) # scale using augmentation intensity
+    images_data.extend([images[i],rotate(images[i], random.uniform(-delta, delta), mode = 'edge'),warp(images[i],transform,output_shape = (32,32),order = 1, mode='wrap')])
+    labels_data.extend([labels[i],labels[i],labels[i]])
+    print(i)
+
+#%%
 import os
 import pickle
 import importlib
@@ -31,11 +143,23 @@ def preprocess_dataset(X):
 
 #%%
 
-(X_test, test_labels) = load_pickled_data("C:/Users/Pierre Lavigne/Desktop/data/test.p", ['features', 'labels'])
-(X_train1, _) = load_pickled_data('C:/Users/Pierre Lavigne/Desktop/data/train2.pickle', ['features', 'labels'])
-(X_train2, _) = load_pickled_data('C:/Users/Pierre Lavigne/Desktop/data/train3.pickle', ['features', 'labels'])
+images_data = np.array(images_data)
+x_train=preprocess_dataset(images_data)
+del images_data
 
-X_train1.extend(X_train2)
+
+(X_test, _) = load_pickled_data("C:/Users/Pierre Lavigne/Desktop/data/test.p", ['features', 'labels'])
+x_test=preprocess_dataset(X_test)
+del X_test
+
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.astype('float32') / 255.
+
+x_train = np.reshape(x_train, (len(x_train), 32, 32, 1))  # adapt this if using `channels_first` image data format
+x_test = np.reshape(x_test, (len(x_test), 32, 32, 1))  # adapt this if using `channels_first` image data format
+
+#%%
+
 #%%
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose
 from keras.models import Model
@@ -65,20 +189,6 @@ autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
 autoencoder.summary()
 
-
-#%%
-import numpy as np
-
-x_train=preprocess_dataset(np.array(X_train1))
-del X_train1
-x_test=preprocess_dataset(X_test)
-del X_test
-
-x_test = x_test.astype('float32') / 255.
-x_train = x_train.astype('float32') / 255.
-
-x_train = np.reshape(x_train, (len(x_train), 32, 32, 1))  # adapt this if using `channels_first` image data format
-x_test = np.reshape(x_test, (len(x_test), 32, 32, 1))  # adapt this if using `channels_first` image data format
 
 #%%
 
