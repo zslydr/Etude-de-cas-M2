57d835c665c8d1482d27dcf2fad44573d7af6200
Romainmarmorat <39861718+Romainmarmorat@users.noreply.github.com>
2018-06-27 20:29:06 +0200

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 17:52:30 2018
+
+@author: Pierre Lavigne
+"""
+
+#%%
+
+import csv
+from skimage.transform import resize
+from skimage import exposure
+from skimage.transform import rotate
+from skimage.transform import warp
+from skimage.transform import ProjectiveTransform
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+
+def readTrafficSigns(rootpath):
+    '''Reads traffic sign data for German Traffic Sign Recognition Benchmark.
+    Arguments: path to the traffic sign data, for example './GTSRB/Training'
+    Returns:   list of images, list of corresponding labels'''
+    images = [] # images
+    labels = [] # corresponding labels
+    # loop over all 42 classes
+    for c in range(0,43):
+        prefix = rootpath + '/' + format(c, '05d') + '/' # subdirectory for class
+        gtFile = open(prefix + 'GT-'+ format(c, '05d') + '.csv') # annotations file
+        gtReader = csv.reader(gtFile, delimiter=';') # csv parser for annotations file
+        next(gtReader) # skip header
+        # loop over all images in current annotations file
+        for row in gtReader:
+            images.append(plt.imread(prefix + row[0])) # the 1th column is the filename
+            labels.append(row[7]) # the 8th column is the label
+        gtFile.close()
+    return(images, labels)
+
+
+def resize_sign(image, new_dim):
+    return(resize(image, new_dim, mode='constant'))
+
+def load_pickled_data(file, columns):
+    with open(file, mode='rb') as f:
+        dataset = pickle.load(f)
+    return tuple(map(lambda c: dataset[c], columns))
+
+def preprocess_dataset(X):
+    #Convert to grayscale, e.g. single Y channel
+    X = 0.299 * X[:, :, :, 0] + 0.587 * X[:, :, :, 1] + 0.114 * X[:, :, :, 2]
+    #Scale features to be in [0, 1]
+    X = (X / 255.).astype(np.float32)
+
+    # Add a single grayscale channel
+    X = X.reshape(X.shape + (1,)) 
+    return X
+
+def flip_extend(X, y):
+    # Classes of signs that, when flipped horizontally, should still be classified as the same class
+    self_flippable_horizontally = np.array([11, 12, 13, 15, 17, 18, 22, 26, 30, 35])
+    # Classes of signs that, when flipped vertically, should still be classified as the same class
+    self_flippable_vertically = np.array([1, 5, 12, 15, 17])
+    # Classes of signs that, when flipped horizontally and then vertically, should still be classified as the same class
+    self_flippable_both = np.array([32, 40])
+    # Classes of signs that, when flipped horizontally, would still be meaningful, but should be classified as some other class
+    cross_flippable = np.array([
+        [19, 20], 
+        [33, 34], 
+        [36, 37], 
+        [38, 39],
+        [20, 19], 
+        [34, 33], 
+        [37, 36], 
+        [39, 38],   
+    ])
+    num_classes = 43
+    
+    X_extended = np.empty([0, X.shape[1], X.shape[2], X.shape[3]], dtype = X.dtype)
+    y_extended = np.empty([0], dtype = y.dtype)
+    
+    for c in range(num_classes):
+        # First copy existing data for this class
+        X_extended = np.append(X_extended, X[y == c], axis = 0)
+        # If we can flip images of this class horizontally and they would still belong to said class...
+        if c in self_flippable_horizontally:
+            # ...Copy their flipped versions into extended array.
+            X_extended = np.append(X_extended, X[y == c][:, :, ::-1, :], axis = 0)
+        # If we can flip images of this class horizontally and they would belong to other class...
+        if c in cross_flippable[:, 0]:
+            # ...Copy flipped images of that other class to the extended array.
+            flip_class = cross_flippable[cross_flippable[:, 0] == c][0][1]
+            X_extended = np.append(X_extended, X[y == flip_class][:, :, ::-1, :], axis = 0)
+        # Fill labels for added images set to current class.
+        y_extended = np.append(y_extended, np.full((X_extended.shape[0] - y_extended.shape[0]), c, dtype = int))
+        
+        # If we can flip images of this class vertically and they would still belong to said class...
+        if c in self_flippable_vertically:
+            # ...Copy their flipped versions into extended array.
+            X_extended = np.append(X_extended, X_extended[y_extended == c][:, ::-1, :, :], axis = 0)
+        # Fill labels for added images set to current class.
+        y_extended = np.append(y_extended, np.full((X_extended.shape[0] - y_extended.shape[0]), c, dtype = int))
+        
+        # If we can flip images of this class horizontally AND vertically and they would still belong to said class...
+        if c in self_flippable_both:
+            # ...Copy their flipped versions into extended array.
+            X_extended = np.append(X_extended, X_extended[y_extended == c][:, ::-1, ::-1, :], axis = 0)
+        # Fill labels for added images set to current class.
+        y_extended = np.append(y_extended, np.full((X_extended.shape[0] - y_extended.shape[0]), c, dtype = int))
+    
+    return (X_extended, y_extended)
+
+def preprocess_dataset_new(X):
+    #Convert to grayscale, e.g. single Y channel
+    X = 0.299 * X[:, :, :, 0] + 0.587 * X[:, :, :, 1] + 0.114 * X[:, :, :, 2]
+    #Scale features to be in [0, 1]
+    X = (X / 255.).astype(np.float32)
+      
+    # Apply localized histogram localization  
+    for i in range(X.shape[0]):
+        X[i] = exposure.equalize_adapthist(X[i])
+    X = X.reshape(X.shape + (1,)) 
+    return X
+
+
+def increase_data(images,nb_int,labels=None):
+    image_size = images[0].shape[0]
+    data_size = images.shape[0]
+    for i in range(data_size):
+        for j in range(nb_int):
+            delta = random.uniform(-20,20) # scale using augmentation intensity
+            d = random.uniform(-10,10)
+            tl_top = random.uniform(-d, d)     # Top left corner, top margin
+            tl_left = random.uniform(-d, d)    # Top left corner, left margin
+            bl_bottom = random.uniform(-d, d)  # Bottom left corner, bottom margin
+            bl_left = random.uniform(-d, d)    # Bottom left corner, left margin
+            tr_top = random.uniform(-d, d)     # Top right corner, top margin
+            tr_right = random.uniform(-d, d)   # Top right corner, right margin
+            br_bottom = random.uniform(-d, d)  # Bottom right corner, bottom margin
+            br_right = random.uniform(-d, d)   # Bottom right corner, right margin
+            transform = ProjectiveTransform()
+            transform.estimate(np.array(((tl_left, tl_top),(bl_left, image_size - bl_bottom),(image_size - br_right, image_size - br_bottom),
+                (image_size - tr_right, tr_top))), np.array(((0, 0),(0, image_size),(image_size, image_size),(image_size, 0))))
+        
+            images = np.append(images,[warp(images[i],transform,output_shape = (32,32),order = 1, mode='edge')],axis = 0)
+            images = np.append(images,[rotate(images[i], random.uniform(-delta, delta), mode = 'edge')],axis=0)
+            if labels is None:
+                pass
+            else:
+                #labels = np.append(labels,labels[i])
+                labels = np.append(labels,labels[i])
+        if i%100 == 0:
+            print(i/data_size*100,'% de progression')
+    return(images,labels)
+
+def sqr_noise(img,x,y,nb_sqr):
+    im = []
+    for k in range(len(img)):
+        img2 = img[k].copy()
+        randomsqrx = np.random.randint(x[0] ,x[1] ,nb_sqr)
+        randomsqry = np.random.randint(y[0] ,y[1] ,nb_sqr)
+        randomsqr = [randomsqrx,randomsqry] 
+        for (i,j) in zip(randomsqr[0],randomsqr[1]) :
+            size = np.random.randint(2,8,2)
+            if (np.random.randint(0,10)<5):
+                img2[i-size[0]+5:i+5,j+5-size[1]:j+5,:] = np.zeros(size[0]*size[1]*3).reshape(size[0],size[1],3)
+            else:
+                img2[i+5-size[0]:i+5,j+5-size[1]:j+5,:] = np.zeros(size[0]*size[1]*3).reshape(size[0],size[1],3) + 255
+        im.append(img2)
+    return np.array(im)
+
+
